cd5a94d8baccaa39e20206e7292f59b3f19d0bfb
William Waters <wmwaters@umich.edu>
2017-10-11 18:59:01 -0400

    work on part b

--- hw6a.py
+++ hw6a.py
@@ -11,10 +11,6 @@ ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter - ')
 html = urlopen(url, context=ctx).read()
-
-# html.parser is the HTML parser included in the standard Python 3 library.
-# information on other HTML parsers is here:
-# http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
 soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
--- hw6b.py
+++ hw6b.py
@@ -1,6 +1,7 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
+import re
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
@@ -13,5 +14,26 @@ soup = BeautifulSoup(html, 'html.parser')
 
 # Retrieve all of the anchor tags
 tags = soup('a')
+i = 0
+
 for tag in tags:
-    print(tag.get('href', None))
+	if i==3:
+		url_list = re.findall('[a-zA-Z0-9]+', str(tag))
+		urlstr = str(url_list[0])
+		print(urlstr)
+		break
+	i+=1
+j = 0
+while j < 4:
+	html = urllib.request.urlopen(urlstr, context=ctx).read()
+	soup = BeautifulSoup(html, 'html.parser')
+	tags = soup('a')
+	i = 0
+	for tag in tags:
+		if i==3:
+			url_list = re.findall('[a-zA-Z0-9]+', str(tag))
+			urlstr = str(url_list[0])
+			print(urlstr)
+			break
+		i+=1
+	j+=1
